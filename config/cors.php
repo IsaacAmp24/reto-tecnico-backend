7d855ed8338b31bbb443a88d7e81449e00b13cca
Jocais Ampudia <ampudiaflores@gmail.com>
2026-03-09 14:27:03 -0500

    Fix CORS allowed_origins by removing trailing slash from URL

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://gregarious-licorice-750477.netlify.app/'],
+    'allowed_origins' => ['https://gregarious-licorice-750477.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
