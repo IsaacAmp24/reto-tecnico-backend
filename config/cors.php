ef757eae7e607f1d08483465eff33fb8411e8365
Jocais Ampudia <ampudiaflores@gmail.com>
2026-03-09 14:43:27 -0500

    Update CORS allowed_origins to include localhost and production URLs

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://gregarious-licorice-750477.netlify.app'],
+    'allowed_origins' => [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'https://gregarious-licorice-750477.netlify.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
